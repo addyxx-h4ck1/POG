f3997b3ef016e22ec1d1bd85ecd8b3be26a8b0c2
addyxx-h4ck1 <briannjosh23@gmail.com>
2023-10-25 16:27:45 +0300

    First Commit

---
+++ Itineraries/validate.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let name = document.getElementById('fname')
+    let email = document.getElementById('email')
+    let budget = document.getElementById('budget')
+    let safari = document.getElementById('safari')
+    let message = document.getElementById('message')
+    let submitbtn = document.querySelector('.submitbtn')
+    let formData = {
+        Name: name.value,
+        Email: email.value,
+        Budget: budget.value,
+        Safari: safari.value,
+        Message: message.value 
+    }
+    
+
+    // https://github.com/github/fetch
+    fetch("https://formsubmit.co/ajax/briannjosh23@gmail.com", {
+    method: "POST",
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+})
+    .then(res =>{
+         name.value = ''
+         email.value= ''
+         budget.value=''
+         message.value = ''
+    }).then (res =>{
+        submitbtn.type = 'button'
+        submitbtn.value = 'Sent!!'
+        submitbtn.style.color = 'green'
+        submitbtn.style.backgroundColor = 'transparent'
+    })
+    
+
+})
+
+
