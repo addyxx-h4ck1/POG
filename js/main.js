d7fc813c4b41a178bd10b04bf7b77c33331f8802
addyxx-h4ck1 <briannjosh23@gmail.com>
2023-10-27 18:02:53 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -44,3 +44,101 @@ const data = fetch('js/images.json')
   
 })
 
+/////////////////fornm/////////
+const formA = document.querySelector('.first-form')
+formA.addEventListener('submit', (e) =>{
+   e.preventDefault();
+   
+   let name = document.getElementById('fname')
+   let email = document.getElementById('femail')
+   let subject = document.getElementById('fsubject')
+   let message = document.getElementById('fmsg')
+   let send = document.querySelector('.fenvoyez')
+
+   let formData = {
+      Name: name.value,
+      Email: email.value,
+      subject: subject.value,
+      Message: message.value
+   }
+   
+
+   fetch("https://formsubmit.co/ajax/briannjosh23@gmail.com", {
+      method: "POST",
+      headers: { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+  })
+      .then(response => response.json())
+      .then(data => {
+         console.log(data)
+
+         name.value = ''
+         email.value = ''
+         subject.value = ''
+         message.value = ''
+   
+         send.value = 'Sent!!!'
+         send.type = 'button'
+         send.style.border = 'none'
+         send.style.color = 'green'
+         send.style.backgroundColor = 'black'
+      })
+      .catch(error => console.log(error));
+
+      
+      
+ })
+
+
+
+const formB = document.querySelector('.second-form')
+formB.addEventListener('submit', (e) =>{
+   e.preventDefault();
+   
+   let name = document.getElementById('sname')
+   let email = document.getElementById('semail')
+   let subject = document.getElementById('ssubject')
+   let message = document.getElementById('smsg')
+   let send = document.querySelector('.senvoyez')
+
+   let formData = {
+      Name: name.value,
+      Email: email.value,
+      subject: subject.value,
+      Message: message.value
+   }
+   
+
+   fetch("https://formsubmit.co/ajax/briannjosh23@gmail.com", {
+      method: "POST",
+      headers: { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+  })
+      .then(response => response.json())
+      .then(data => {
+         console.log(data)
+
+         name.value = ''
+         email.value = ''
+         subject.value = ''
+         message.value = ''
+   
+         send.value = 'Sent!!!'
+         send.type = 'button'
+         send.style.border = 'none'
+         send.style.color = 'green'
+         send.style.backgroundColor = 'black'
+      })
+      .catch(error => console.log(error));
+
+      
+      
+ })
+
+
